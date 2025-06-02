3062a8a235b67dc6f5a67e833051223a3af5ed8e
ɢᴏᴘᴀʟ ᴋʀɪᴤʜɴᴀ ᴍᴏʜᴀᴘᴀᴛʀᴀ <110560006+xtocodex@users.noreply.github.com>
2025-06-02 22:44:40 +0000

    comparision and equalitycheck

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+//console.log("2" > 1);
+//console.log("02" > 1);
+
+
+console.log(null > 0);
+
+console.log(null == 0);
+
+console.log(null >= 0); 
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// comparision and equalitycheck are different in js....
+// === triple check
+
+console.log("2" === 2);
