3155ade027221c9e395e001e46fbfabfeac81d6f
ɢᴏᴘᴀʟ ᴋʀɪᴤʜɴᴀ ᴍᴏʜᴀᴘᴀᴛʀᴀ <110560006+xtocodex@users.noreply.github.com>
2025-06-02 22:32:32 +0000

    conversionOperations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,5 +22,53 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+//console.log(typeof stringNumber);
+
+
+
+
+// ************************ Operations ************************
+
+let value = 3
+
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/2);
+//console.log(2%2);
+
+let str1 = "hello"
+let str2 = "gopal"
+
+let str3 = str1 + str2
+//console.log(str3);
+//
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+//
+//console.log( 3+ 4 * 5 % 3);
+
+// never try to complicate code.. always try to make it clean and simple not messy/confusing.
+
+
+
+//console.log(+true);
+//console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter; // pre-fix
+gameCounter++; // post-fix
+
+console.log(gameCounter);
+
 
