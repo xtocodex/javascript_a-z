bdef99b1356df25b47682c74dd5c2d222f396ae1
ɢᴏᴘᴀʟ ᴋʀɪᴤʜɴᴀ ᴍᴏʜᴀᴘᴀᴛʀᴀ <110560006+xtocodex@users.noreply.github.com>
2025-06-02 21:00:15 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using Nodejs, not browser so this won't work here now,,later we will know how.
+
+console.log(3 +3); 
+
+console.log("gopal") 
+
+// always try to maintain code readability simple,clean..
+
+// refer MDN and original tc.39.es or EcmaScript ..for document reference.
+
+let name = "gopal"
+let age = 18
+let isLoggedIn = true
+let state = null 
+
+// number => 2 to power 53 
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object 
+
+console.log(typeof  undefined); //undefined
+console.log(typeof null); // object 
+
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,25 @@
-console.log("Gopal");
+const accountId = 144553
+let accountEmail = "gopal@google.com"
+var accountPassword = "12345" 
+accountCity = "Balasore"
+let accountState;
+// accountId = 2 // this type of writing without declaring not allowed.
+
+/* 
+Prefer not to use -- 'var'
+bcz of issue in block scope and functional scope.
+*/
+
+
+accountEmail = "gm@gm.com"
+accountPassword = "1122334455"
+accountCity = "Bengaluru"
+
+
+
+console.table( [accountId, accountEmail, accountPassword, accountCity, accountState] );
+
+
+
+
+
