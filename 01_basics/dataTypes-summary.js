363bcb95682c36f2271c9ed0415d2c3672327e67
ɢᴏᴘᴀʟ ᴋʀɪᴤʜɴᴀ ᴍᴏʜᴀᴘᴀᴛʀᴀ <110560006+xtocodex@users.noreply.github.com>
2025-06-02 23:51:31 +0000

    stack-heaps more

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -1,5 +1,7 @@
 // Primitive 
 
+
+
 // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
 const score = 100
@@ -20,7 +22,8 @@ const bigNumber = 3444444444545666669n
 
 
 // Refernce (Non-primitive)
-// Array, Objects, Functions
+
+// --> Array, Objects, Functions
 
 const heros = ["shaktiman", "naagraj", "doga"]
 
@@ -41,3 +44,33 @@ console.log(typeof heros);
 console.log(typeof anotherId);
 
 // check above all typeof for all operators ... for interviews 
+
+
+
+// 262.ecma-international.org/5.1/#sec-11.4.3
+
+// ++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitve), Heap (Non-primitive)
+
+let myYoutubename = "jarvis"
+
+let anothername = myYoutubename
+anothername = "xeon"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {  
+    email: "user@user.com" ,
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "gopal@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
